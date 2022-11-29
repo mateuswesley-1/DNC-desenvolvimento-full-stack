a319187b27f91ddc925cbd0f2339d54b3ec06fc9
Mateus Wesley <mateuswesley314@gmail.com>
2022-11-29 16:36:40 -0300

    jogo advinhacao js

---
+++ Javascript/Jogo advinhação/script.js
@@ -0,0 +1,42 @@
+// sortear um valor entre 0 e 9
+// verificar se é igual ao valor escolhido
+// se for igual, finaliza o jogo
+// senão, continua, contando cada erro para exibir no final
+var num_random = Math.floor(Math.random()*10)
+var num_tentativas = 0
+
+function sortear(){
+    var num_user = document.querySelector('input#inum')
+    var mensagem = document.querySelector('h2#textoFim')
+    var alerta = document.querySelector('p#mensagem')
+
+    if(num_user.value > 9 || num_user.value < 0 ){
+        alerta.style.color = 'red'
+    }else{
+        alerta.style.color = 'black'
+        if(num_random == num_user.value){
+            tela1.style.display = 'none'
+            tela2.style.display = 'block'
+
+            num_tentativas += 1
+            mensagem.innerHTML = `Você acertou em ${num_tentativas} tentativas!`
+        }else{
+            num_tentativas += 1
+            num_user.style.border = '2px solid red'
+        }
+
+    }
+}
+
+function jogarNovamente(){
+    //sorteia um novo número e reseta num de tentativar
+    num_random = Math.floor(Math.random()*10)
+    num_tentativas = 0
+
+    var num_user = document.querySelector('input#inum')
+
+    num_user.value = ''
+    num_user.style.border = ''
+    tela1.style.display = 'block'
+    tela2.style.display = 'none'
+}
