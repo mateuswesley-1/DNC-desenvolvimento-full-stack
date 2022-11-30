f6626ea7f877c7afdce5710fa945c94f125941f6
Mateus Wesley <mateuswesley314@gmail.com>
2022-11-30 17:40:25 -0300

    refatorando

--- Javascript/Jogo advinhação/script.js
+++ Javascript/Jogo advinhação/script.js
@@ -1,17 +1,21 @@
-// sortear um valor entre 0 e 9
-// verificar se é igual ao valor escolhido
-// se for igual, finaliza o jogo
-// senão, continua, contando cada erro para exibir no final
+// Variáveis
 let num_random = Math.floor(Math.random()*10)
 let num_tentativas = 0
 
+const btn2 = document.querySelector('button#btnJogar')
+const btn1 = document.querySelector('button#btnSorteio')
 const num_user = document.querySelector('input#inum')
-const mensagem = document.querySelector('h2#textoFim')
+const mensagem = document.querySelector('div.textoFim')
 const alerta = document.querySelector('p#mensagem')
 const tela1 = document.querySelector('.tela1')
 const tela2 = document.querySelector('.tela2')
 const feedback = document.querySelector('p#tentativas')
 
+// Métodos
+function toggleTela(){
+    tela1.classList.toggle('hide')
+    tela2.classList.toggle('hide')
+}
 function sortear(event){
     // nao enviar formulario quando apertamos enter
     event.preventDefault()
@@ -25,20 +29,17 @@ function sortear(event){
         alerta.style.color = 'black'
 
         if(num_random == num_user.value){
-            tela1.classList.add('hide')
-            tela2.classList.remove('hide')
-
-            mensagem.innerText = `Você acertou em ${num_tentativas} tentativas!`
+            toggleTela()
+            mensagem.innerHTML = `<p>Você acertou em ${num_tentativas} tentativas!</p>
+            <p>Número Sorteado: ${num_user.value}</p>`
 
         }else{
-
             num_user.style.border = '2px solid red'
             feedback.innerText = `Tentativas: ${num_tentativas}`
         }
 
     }
 }
-
 function jogarNovamente(){
     //sorteia um novo número e reseta num de tentativar
     num_random = Math.floor(Math.random()*10)
@@ -46,23 +47,20 @@ function jogarNovamente(){
 
     num_user.value = ''
     num_user.style.border = ''
-    tela1.classList.remove('hide')
-    tela2.classList.add('hide')
+    toggleTela()
+}
+function substituirImput(){
+    num_user.value = num_user.value.replace(/[^0-9]/g,'')
 }
 
-/* Event Listeners*/
-
-/* Sorteio*/
-const btn1 = document.querySelector('button#btnSorteio')
-
+//Eventos
 btn1.addEventListener('click', sortear)
-
-/* Jogar novament*/
-const btn2 = document.querySelector('button#btnJogar')
-
 btn2.addEventListener('click', jogarNovamente)
+num_user.addEventListener('input', substituirImput)
 
-/* Só permitir input numericos */
-num_user.addEventListener('input', function(){
-    num_user.value = num_user.value.replace(/[^0-9]/g,'')
+document.addEventListener('keydown', function(e){
+    if (e.key == 'Enter' && tela1.classList.contains('hide')){
+        jogarNovamente()
+    }
 })
+
