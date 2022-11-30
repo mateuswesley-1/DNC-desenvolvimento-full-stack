8178bcd53aef5daa829f6ce223e0af7d398c9110
Mateus Wesley <mateuswesley314@gmail.com>
2022-11-30 11:22:16 -0300

    versao final professor

--- Javascript/Jogo advinhação - Versão Professor/script.js
+++ Javascript/Jogo advinhação - Versão Professor/script.js
@@ -1,4 +1,7 @@
 const randomNumber = Math.round(Math.random()*10)
+const tela1 = document.querySelector('.tela1')
+const tela2 = document.querySelector('.tela2')
+const feedback = document.querySelector('p#tentativas')
 let numTentativas = 0
 
 function sortear(){
@@ -7,16 +10,28 @@ function sortear(){
 
     // adiciona a classe hide a caixa que recebe o número e
     if(userNumber.value == randomNumber){
-        document.querySelector('.tela1').classList.add('hide')
-        document.querySelector('.tela2').classList.remove('hide')
+        tela1.classList.add('hide')
+        tela2.classList.remove('hide')
         document.querySelector('#textoFim').innerText = `Você acertou em ${numTentativas} tentativas`
 
     }else{
-        document.querySelector('p#tentativas').innerHTML = `Tentativas: ${numTentativas}`
+        feedback.innerText = `Tentativas: ${numTentativas}`
     }
 
 }
 
-function jogarNovamente(){
+/* Events */
 
-}
+btn1 = document.querySelector('input#btnTry')
+
+btn1.addEventListener('click', sortear)
+
+
+btn2 = document.querySelector('input#btnAgain')
+
+btn2.addEventListener('click', function(){
+    numTentativas = 0
+    tela1.classList.remove('hide')
+    tela2.classList.add('hide')
+    feedback.innerText = `Tentativas: ${numTentativas}`
+})
