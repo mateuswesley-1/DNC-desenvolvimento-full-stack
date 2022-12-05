f5478ef35dd1bf7d9d19c68cae365f46f146fc3b
Mateus Wesley <mateuswesley314@gmail.com>
2022-12-05 13:21:28 -0300

    Update ex013.rb
    comentarios

--- Ruby/Exercicios/ex013.rb
+++ Ruby/Exercicios/ex013.rb
@@ -1,10 +1,15 @@
 
 
-
+# gera uma lista das dezenas entre 30 e 100
 lista_limites = (30..100).step(10).to_a
+
+# sorteia um dos limites da lista
 velocidade_limite = lista_limites[rand(lista_limites.length)]
+
+# menor velocidade possível: 30, maior velocidade:150
 velocidade = 30 + rand(150)
 
+# verifica se o veiculo respeitou o limite de velocidade e calcula a multa caso não tenha respeitado
 if velocidade > velocidade_limite
   excesso = velocidade-velocidade_limite
   puts "Sua velocidade: #{velocidade} km/h\nLimite: #{velocidade_limite} km/h"
